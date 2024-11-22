ef56183775436a6f6141175167bd68480d8c5a6e
davidtom1 <programmingwithdavidtom@post.bgu.ac.il>
2024-11-22 16:41:25 +0000

    settelment

---
+++ src/Settelment.cpp
@@ -0,0 +1,15 @@
+#include "Settelment.h"
+Settlement(const string &name, SettlementType type)
+: const string name =&name, SettlementType =type
+{
+
+};
+const string &getName() const{
+    return this.&name
+}:
+SettlementType getType() const{
+    return this.type;
+};
+const string toString() const{
+    return string s(this.&name "," this.type)
+};
