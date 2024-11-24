cd67fbb24ffb64ad5309c5206c35fc62222dfda8
davidtom1 <programmingwithdavidtom@post.bgu.ac.il>
2024-11-24 07:57:34 +0000

    changes with metargel

--- src/Facility.cpp
+++ src/Facility.cpp
@@ -1,64 +1,49 @@
 #include "Facility.h"
 using namespace std;
 
-class FacilityType {
-    protected:
-    const string name;
-    const FacilityCategory category;
-    const int price;
-    const int lifeQuality_score;
-    const int economy_score;
-    const int environment_score;
-    const int score[3]; //added feild
 
-    public:
 // FacilityType:
-        FacilityType(const string &name, const FacilityCategory category, const int price, const int lifeQuality_score, const int economy_score, const int environment_score):
+        FacilityType::FacilityType(const string &name, const FacilityCategory category, const int price, const int lifeQuality_score, const int economy_score, const int environment_score):
         name(name), category(category), price(price), lifeQuality_score(lifeQuality_score), economy_score(economy_score), environment_score(environment_score),score({lifeQuality_score,economy_score,environment_score})
         {}
-        const string &getName() const{
+        const string & FacilityType::getName() const{
             return name;
         }
-        int getCost() const{
+        int  FacilityType::getCost() const{
             return price;
         }
-        int getLifeQualityScore() const{
+        int  FacilityType::getLifeQualityScore() const{
             return lifeQuality_score;
         }
-        int getEnvironmentScore() const{
+        int  FacilityType::getEnvironmentScore() const{
             return environment_score;
         }
-        int getEconomyScore() const{
+        int  FacilityType::getEconomyScore() const{
             return economy_score;
         }
-        FacilityCategory getCategory() const{
+        FacilityCategory  FacilityType::getCategory() const{
             return category;
         }
- };
-        //Facility:
-class Facility {
-    private:
-        const string settlementName;
-        FacilityStatus _status;
-        int timeLeft;
-    
-    public:
-        Facility(const string &name, const string &settlementName, const FacilityCategory category, const int price, const int lifeQuality_score, const int economy_score, const int environment_score);
 
-        const string &getSettlementName() const{
+        //Facility:
+
+    public:
+        Facility::Facility(const string &name, const string &settlementName, const FacilityCategory category, const int price, const int lifeQuality_score, const int economy_score, const int environment_score);
+
+        const string &Facility::getSettlementName() const{
             return  settlementName;
         }
-        const int getTimeLeft() const{
+        const int Facility::getTimeLeft() const{
             return timeLeft;
         }
-        FacilityStatus step(){//not sure this is correct
+        FacilityStatus Facility::step(){//not sure this is correct
             timeLeft--;
         }
-        void setStatus(FacilityStatus status){
+        void Facility::setStatus(FacilityStatus status){
            _status=status;
         }
-        const FacilityStatus& getStatus() const{
+        const FacilityStatus& Facility::getStatus() const{
             return _status;
         }
         
-};
+
