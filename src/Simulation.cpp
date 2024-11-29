cc7aa27c54a1cf3cb60ed6dcf96e33079f0c3d12
davidtom1 <programmingwithdavidtom@post.bgu.ac.il>
2024-11-29 15:20:31 +0000

    simulation

--- src/Simulation.cpp
+++ src/Simulation.cpp
@@ -26,8 +26,53 @@ void Simulation::start(){
      while (isRunning){
 
      }
-  
 }
+
+void Simulation::addPlan(const Settlement *settlement, SelectionPolicy *selectionPolicy){
+     Plan p(planCounter, *settlement,selectionPolicy,this->facilitiesOptions);
+     plans.push_back(p);
+     planCounter++;
+}
+void Simulation::addAction(BaseAction *action){
+     actionsLog.push_back(action);
+}
+
+bool Simulation::addSettlement(Settlement *settlement){
+        bool ans=true;
+        int i=0;
+        while(i < settlements.size() && ans){
+             i++;
+             if (settlements[i]->getName()==settlement->getName())
+                ans=false;
+        }
+        if(ans)
+             settlements.push_back(settlement);
+}
+
+bool Simulation::addFacility(FacilityType facility){
+        bool ans=true;
+        int i=0;
+        while(i< facilitiesOptions.size()&& ans){
+                i++;
+                if (facilitiesOptions[i].getName()== facility.getName())
+                    ans=false; 
+        }
+        if(ans)
+        facilitiesOptions.push_back(facility);
+}
+
+bool Simulation::isSettlementExists(const string &settlementName){
+        bool ans=true;
+        int i=0;
+        while(i < settlements.size() && ans){
+             i++;
+             if (settlements[i]->getName()==settlementName)
+                ans=false;
+        }
+        return ans;
+}
+
+
 
 void Simulation::close(){
         isRunning=false;
