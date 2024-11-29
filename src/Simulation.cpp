2bbf511c5a246210f4a98866e24f8b9fff84cf36
davidtom1 <programmingwithdavidtom@post.bgu.ac.il>
2024-11-29 10:49:45 +0000

    simulation header

--- src/Simulation.cpp
+++ src/Simulation.cpp
@@ -4,11 +4,16 @@
 #include "Plan.h"
 #include "Settlement.h"
 #include <iostream>
+#include "Simulation.h"
 using std::string;
 using std::vector;
 
 class BaseAction;
 class SelectionPolicy;
+
+Simulation::Simulation(const string &configFilePath):isRunning(true),
+planCounter(1), actionLog(), Plans(),settlements(),facilityOptions(){}
+
 
 void Simulation::start(){
      std::cout << "the simulation has started";
